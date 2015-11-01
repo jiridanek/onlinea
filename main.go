28b0aee8145794bad67980e98c71c305817575e7
Jirka Daněk <dnk@mail.muni.cz>
2015-11-01 18:32:52 +0100

    update Duolingo stuff for fall 2015

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
 import (
-    "os"
-    "fmt"
+	"fmt"
+	"os"
 )
 
 func Onlinea(args []string) {
-        fmt.Println("Onlinea")
+	fmt.Println("Onlinea")
 }
 
 func Help(args []string) {
-        fmt.Println("Help")
+	fmt.Println("Help")
 }
 
 func main() {
-    args := os.Args
-    if len(args) <= 1 {
-        Onlinea(args)
-        return
-    }
-    switch args[1] {
-    case "duolingo":
-        Duolingo(args[2:])
-    default:
-        Help(args)
-    }
+	args := os.Args
+	if len(args) <= 1 {
+		Onlinea(args)
+		return
+	}
+	switch args[1] {
+	case "duolingo":
+		Duolingo(args[2:])
+	case "blok":
+		Blok(args[2:])
+	case "englishcentral":
+		EnglishCentral(args[2:])
+	default:
+		Help(args)
+	}
 }
